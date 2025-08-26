72710ace483f006f2c9d903fa924eaef8bc5a012
Raul Gonzalez <speedygonzales77@gmail.com>
2025-08-26 08:31:00 -0500

    update

--- push.js
+++ push.js
@@ -1,16 +1,19 @@
-var push = require('web-push')
+const webpush = require('web-push');
 
-//let vapidkeys = push.generateVAPIDKeys();
+// VAPID keys should only be generated only once.
+const vapidKeys = {
+  publicKey:
+    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
+  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+};
 
-//console.log(vapidkeys);
+webpush.setVapidDetails(
+  'mailto:example@yourdomain.org',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
 
-let vapidkeys = {
-  publicKey: 'BCis9afWX-SRKt021KIJwH7M2I67P0dlDBEvIdaeRCRj6GbPMqwG0V9rR6XPmaPCDmOT6oVObmYLEMba3oW5sMU',
-  privateKey: 'dTraF3I4r-hbZ0tE3CYVYFGoz1woSrN-O5XpPJNOI_c'
-}
+// This is the same output of calling JSON.stringify on a PushSubscription
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/dloNXXys3Bg:APA91bEgg0TyJ6LWZtB-MryobcHA18Fzq3TNM1LQ-RwHyTpCbrGGf0AMi8SmjmfeaHsAhXKE9zSgoNGCR4PNn3UXKlChSjRqGDyGurr1qBiRfvEPrAJz3d08jc75xxWzMs14--KRFvrA","expirationTime":null,"keys":{"p256dh":"BBhBa3D6GSVI4fVLLLRcK4rkYjkzjwZxOP-_OFy9Lrm6h8mMBP3-N_GrJ-2cFOCRJ6BKuTWsxwq5L1L5CcdN3hI","auth":"hYwsO8Jnryz2bjmPomsGGg"}}
 
-push.setVapidDetails('mailto:speedygonzales77@gmail.com', vapidkeys.privateKey, vapidkeys.privateKey)
-
-let sub= {"endpoint":"https://fcm.googleapis.com/fcm/send/dloNXXys3Bg:APA91bEgg0TyJ6LWZtB-MryobcHA18Fzq3TNM1LQ-RwHyTpCbrGGf0AMi8SmjmfeaHsAhXKE9zSgoNGCR4PNn3UXKlChSjRqGDyGurr1qBiRfvEPrAJz3d08jc75xxWzMs14--KRFvrA","expirationTime":null,"keys":{"p256dh":"BBhBa3D6GSVI4fVLLLRcK4rkYjkzjwZxOP-_OFy9Lrm6h8mMBP3-N_GrJ-2cFOCRJ6BKuTWsxwq5L1L5CcdN3hI","auth":"hYwsO8Jnryz2bjmPomsGGg"}}
-
-push.sendNotification(SubmitEvent, 'tet message')
+webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
