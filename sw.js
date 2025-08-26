29514f8357695ac2355b6d39b82481585d3ff65d
Raul Gonzalez <speedygonzales77@gmail.com>
2025-08-26 09:51:55 -0500

    updates

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
+
+      const options = {
+        body: "Registration is now open - 4",
+        icon: "https://www.blinn.edu/_files/images/ico/favicon.ico", // Optional: path to an icon image
+        tag: "unique-notification-id-4", // Optional: for grouping/replacing notifications
+      };
+
 self.addEventListener('push', () => {
   self.registration.showNotification('Hello world-4!', options);
 });
