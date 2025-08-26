336b09d57e89c25db7781c56f8236cbc614741f2
Raul Gonzalez <speedygonzales77@gmail.com>
2025-08-26 09:53:27 -0500

    updates

--- push.js
+++ push.js
@@ -13,4 +13,4 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/cu_jAN_t9no:APA91bFSEOPRFBqIhPkrCpZwGx9h0dZR5e2B6VXhEPc8w0G5DWlWXmfKCEbNW8h2XTasDLjBQBLJx2CNeYw_ID_cQYmNF4E4H_NqyVnCMUUshwwrD51_Kaa6wXw-z2cY49r389DuIcvJ","expirationTime":null,"keys":{"p256dh":"BLImOXVYWQ1oPpuijQ_tEsFBo13M0KJW_dOTMMsa19vDWB75sedohXOgWlp2qFwMoxuxYPLer7F6A94D3kO9-Kg","auth":"ArCClvAEj_LJMTYe_n6U1w"}}
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/d0z_NWD0wTM:APA91bFiAu37GwviHQKyb8BP8riOGL6shVYwzep7wY151CsucmC0YCR8-_30rRJsekeTPhbw7VApAqT5_jL2_WxHKWRgbKqaT1AX9gfN3gZMSJ0iRHfcfBePF9cQ7p4mhfdJ7y6ZrMvV","expirationTime":null,"keys":{"p256dh":"BGFJw72kwQttzxdRhmrcpwPvToKu7F5H5cjgxjRvDpf-zxxBgpAmmvt_abTub5_eLgfxaf1PvXdxaTk99TVlxO4","auth":"7AIbadTCTFFziDpsh1ALdA"}}
